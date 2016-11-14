5d42dd63e7758fb3f6007be7dcdf2a0cac36472c
Dan <danielpowell12@msn.com>
2016-11-13 18:37:13 -0700

    Places sample RidgePie and RidgeBar charts at root

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import RidgeBar from './Bar.js';
+import RidgePie from './Pie.js';
+import StudentsPerYear from './mock_students_per_year.js'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RidgeBar data={StudentsPerYear}
+                  x_attr='grad_year'
+                  y_attr='student_count'
+        />
+        <RidgePie data={StudentsPerYear}
+                  dimension='student_count'
+        />
       </div>
     );
   }
