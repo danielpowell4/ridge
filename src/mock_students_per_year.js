d6832433f00ab8b74255258bd16a00e108c2dbfb
Dan <danielpowell12@msn.com>
2016-11-13 18:31:12 -0700

    Adds mocked students per grad year data

---
+++ src/mock_students_per_year.js
@@ -0,0 +1,24 @@
+let StudentsPerYear = [{
+  "grad_year": 2014,
+  "student_count": 42
+},{
+  "grad_year": 2015,
+  "student_count": 102
+},{
+  "grad_year": 2016,
+  "student_count": 160
+},{
+  "grad_year": 2017,
+  "student_count": 82
+},{
+  "grad_year": 2018,
+  "student_count": 48
+},{
+  "grad_year": 2019,
+  "student_count": 68
+},{
+  "grad_year": 2020,
+  "student_count": 28
+}];
+
+export default StudentsPerYear;
