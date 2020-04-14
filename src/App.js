3c5626182b2d38911bc91016bfa3565fb7825269
Dan <danielpowell12@msn.com>
2020-04-13 19:01:03 -0600

    Adds gh-pages

--- src/App.js
+++ src/App.js
@@ -16,20 +16,23 @@ import {
 
 const Landing = _ => <div>Select a category...</div>;
 
+const baseUrl = `/ridge`; // for gh-pages
+
 const App = _ => (
   <Router>
     <div style={{ marginBottom: "2rem" }}>
       <MainMenu />
       <main style={{ padding: "1rem 0" }}>
-        <Route path="/employees" component={Employees} />
-        <Route path="/tasks" component={Tasks} />
-        <Route path="/column" component={Column} />
-        <Route path="/histogram" component={Histogram} />
-        <Route path="/timegraph" component={Timegraph} />
-        <Route path="/lesson-turnaround" component={SummaryTurnaround} />
-        <Route path="/material-requests" component={MaterialRequests} />
-        <Route path="/referrals" component={Referrals} />
-        <Route path="/projections" component={Projections} />
+        <Route path={`${baseUrl}/employees`} component={Employees} />
+        <Route path={`${baseUrl}/tasks`} component={Tasks} />
+        <Route path={`${baseUrl}/column`} component={Column} />
+        <Route path={`${baseUrl}/histogram`} component={Histogram} />
+        <Route path={`${baseUrl}/timegraph`} component={Timegraph} />
+        <Route path={`${baseUrl}/lesson-turnaround`} component={SummaryTurnaround} />
+        <Route path={`${baseUrl}/material-requests`} component={MaterialRequests} />
+        <Route path={`${baseUrl}/referrals`} component={Referrals} />
+        <Route path={`${baseUrl}/projections`} component={Projections} />
+        <Route exact path={baseUrl} component={Landing} />
         <Route exact path="/" component={Landing} />
       </main>
     </div>
