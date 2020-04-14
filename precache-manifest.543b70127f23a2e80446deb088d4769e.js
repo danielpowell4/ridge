f1ffa519bf9da2389896b45260bf10c7053e1bdf
Dan <danielpowell12@msn.com>
2020-04-14 10:48:24 -0600

    Updates

---
+++ precache-manifest.543b70127f23a2e80446deb088d4769e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef5cf4a8bea97e7ddacc",
+    "url": "/ridge/static/css/main.5216ebf8.chunk.css"
+  },
+  {
+    "revision": "ef5cf4a8bea97e7ddacc",
+    "url": "/ridge/static/js/main.43e81c2b.chunk.js"
+  },
+  {
+    "revision": "55707000844bfef0796c",
+    "url": "/ridge/static/js/runtime~main.10b47215.js"
+  },
+  {
+    "revision": "52ef0b20614416f70da9",
+    "url": "/ridge/static/js/2.10db3c2d.chunk.js"
+  },
+  {
+    "revision": "904c0b30c76c75fa824077b0c6e31ae8",
+    "url": "/ridge/index.html"
+  }
+];
