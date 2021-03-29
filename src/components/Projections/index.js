9b09a90c8164943bb35d81a1229d4562c42582a7
Dan <danielpowell12@msn.com>
2021-03-29 12:32:16 -0600

    Reformats data
    This takes a step towards making this a bit more battle-ready.
    
    Big diff, small(ish) change including:
    
    - pulling up the fallback 'no data' message to a guard return
    - using titleize util helper on strings

--- src/components/Projections/index.js
+++ src/components/Projections/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import moment from "moment";
+import { titleize } from "./utils";
 import { BarTime, ColorLabel } from "./charts";
 import Filters from "./Filters";
 
@@ -54,90 +55,30 @@ const categoryOptions = [
 ].map((cat) => ({ value: cat, label: cat }));
 
 const dimensionOptions = [
-  { value: "hours", label: "Total hours", },
-  { value: "at_client_hours", label: "At Client Hours", },
-  { value: "pp_office_hours", label: "PP Office Hours", },
-  { value: "online_hours", label: "Online Hours", },
-  { value: "coaches", label: "Coaches", },
-  { value: "all_students", label: "All students", },
-  { value: "High School Senior", label: "High School Senior", },
-  { value: "High School Junior", label: "High School Junior", },
-  { value: "High School Sophomore", label: "High School Sophomore", },
-  { value: "High School Freshman", label: "High School Freshman", },
-  { value: "College Senior", label: "College Senior", },
-  { value: "College Junior", label: "College Junior", },
-  { value: "College Sophomore", label: "College Sophomore", },
-  { value: "College Freshman", label: "College Freshman", },
-  { value: "Grade 1", label: "Grade 1", },
-  { value: "Grade 2", label: "Grade 2", },
-  { value: "Grade 3", label: "Grade 3", },
-  { value: "Grade 4", label: "Grade 4", },
-  { value: "Grade 5", label: "Grade 5", },
-  { value: "Grade 6", label: "Grade 6", },
-  { value: "Grade 7", label: "Grade 7", },
-  { value: "Grade 8", label: "Grade 8", },
-  { value: "Kindergarten", label: "Kindergarten", },
-];
-
-const Filters = ({ filters }) => (
-  <ul
-    style={{
-      display: `grid`,
-      gridGap: `1rem`,
-      maxWidth: 600,
-      margin: `auto`,
-      listStyle: `none`,
-    }}
-  >
-    {filters.map((filter) => (
-      <li key={filter.label}>
-        <span>
-          <strong>{filter.label}</strong>
-          {!!filter.onSelectAll && (
-            <button onClick={filter.onSelectAll}>Select All</button>
-          )}
-        </span>
-
-        {filter.type === `Select` ? (
-          <Select
-            options={filter.options}
-            selected={filter.selected}
-            onChange={filter.onChange}
-          />
-        ) : filter.type === `date` ? (
-          <input
-            type={filter.type}
-            value={filter.selected}
-            onChange={filter.onChange}
-          />
-        ) : filter.type === `checkboxGroup` ? (
-          <div
-            style={{ display: `grid`, gridTemplateColumns: `repeat(2, 1fr)` }}
-          >
-            {filter.options.map((opt) => (
-              <label key={opt.value} htmlFor={opt.value}>
-                <input
-                  type="checkbox"
-                  id={opt.value}
-                  value={opt.value}
-                  checked={
-                    !!filter.selected.find(
-                      (selected) => selected.value === opt.value
-                    )
-                  }
-                  onChange={filter.onChange}
-                />
-                {opt.label}
-              </label>
-            ))}
-          </div>
-        ) : (
-          <p style={{ color: `red` }}>Unsupported filter type</p>
-        )}
-      </li>
-    ))}
-  </ul>
-);
+"hours",
+"at_client_hours",
+"pp_office_hours",
+"online_hours",
+"coaches",
+"all_students",
+"High School Senior",
+"High School Junior",
+"High School Sophomore",
+"High School Freshman",
+"College Senior",
+"College Junior",
+"College Sophomore",
+"College Freshman",
+"Grade 1",
+"Grade 2",
+"Grade 3",
+"Grade 4",
+"Grade 5",
+"Grade 6",
+"Grade 7",
+"Grade 8",
+"Kindergarten",
+].map(dimension => ({ value: dimension, label: titleize(dimension)}));
 
 // actual component
 
@@ -252,24 +193,14 @@ const Projections = () => {
     });
   }
 
-  let marketLabel = "";
-  let categoryLabel = "";
-  if (activeData.length) {
-    if (markets.length === marketOptions.length) {
-      marketLabel = `all markets`;
-    } else if (markets.length > 3) {
-      marketLabel = `${markets.length} markets`;
-    } else {
-      marketLabel = markets.map((m) => m.label).join(", ");
-    }
-
-    if (categories.length === categoryOptions.length) {
-      categoryLabel = `all categories`;
-    } else if (categories.length > 3) {
-      categoryLabel = `${categories.length} categories`;
-    } else {
-      categoryLabel = categories.map((c) => c.label).join(", ");
-    }
+  if (!activeData.length) {
+    return (
+      <div>
+        <Filters filters={filters} />
+        <hr style={{ maxWidth: 660, margin: `1rem auto` }} />
+        <div style={{ textAlign: `center` }}>Select filters</div>
+      </div>
+    )
   }
 
   const fullRange = activeData.map((d) => d.y);
@@ -318,130 +249,144 @@ const Projections = () => {
     ? dataSegmentTwo.map((d) => d.y)
     : [0];
 
+  let marketLabel = "";
+  let categoryLabel = "";
+  if (activeData.length) {
+    if (markets.length === marketOptions.length) {
+      marketLabel = `All markets`;
+    } else if (markets.length > 3) {
+      marketLabel = `${markets.length} markets`;
+    } else {
+      marketLabel = markets.map((m) => m.label).join(", ");
+    }
+
+    if (categories.length === categoryOptions.length) {
+      categoryLabel = `All categories`;
+    } else if (categories.length > 3) {
+      categoryLabel = `${categories.length} categories`;
+    } else {
+      categoryLabel = categories.map((c) => c.label).join(", ");
+    }
+  }
+  const chartTitle = `${titleize(dimension)} for ${categoryLabel} in ${marketLabel}`
+
   return (
     <div>
       <Filters filters={filters} />
       <hr style={{ maxWidth: 660, margin: `1rem auto` }} />
-      {!activeData.length ? (
-        <div style={{ textAlign: `center` }}>Select filters</div>
-      ) : (
-        <>
-          <section>
-            <div
-              style={{
-                width: `100%`,
-                maxWidth: `1040px`,
-                display: `flex`,
-                flexFlow: `row wrap`,
-                margin: `auto`,
-              }}
-            >
-              <h1>{`${dimension} for ${categoryLabel} in ${marketLabel}`}</h1>
-              <BarTime dataset={activeData} width={800} height={300} />
-              <aside
-                style={{
-                  display: `flex`,
-                  flexFlow: `row wrap`,
-                  justifyContent: `center`,
-                  padding: `.5rem 1rem`,
-                  margin: `auto`,
-                }}
-              >
-                <ColorLabel dataset={activeData} />
-              </aside>
-            </div>
-          </section>
-          <section>
-            <div
-              style={{
-                width: `100%`,
-                maxWidth: `1040px`,
-                display: `flex`,
-                flexFlow: `row wrap`,
-                margin: `auto`,
-              }}
-            >
-              <div style={{ width: "48%" }}>
-                <BarTime
-                  width={400}
-                  height={200}
-                  dataset={dataSegmentOne}
-                  fullRange={fullRange}
-                  timeTickFormat={(t) => moment(t).format("M/D")}
-                />
-              </div>
-              <div style={{ width: "48%" }}>
-                <BarTime
-                  width={400}
-                  height={200}
-                  dataset={dataSegmentTwo}
-                  fullRange={fullRange}
-                  timeTickFormat={(t) => moment(t).format("M/D")}
-                />
-              </div>
-              <h2 style={{ textAlign: `center` }}>
-                {`${dimension} for ${categoryLabel} in ${marketLabel}`} | Simple
-                Stat Breakdown
-              </h2>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Stat</th>
-                    <th>{`${segmentOneStart.format(
-                      "L"
-                    )} - ${segmentOneEnd.format("L")}`}</th>
-                    <th>{`${segmentTwoStart.format(
-                      "L"
-                    )} - ${segmentTwoEnd.format("L")}`}</th>
-                    <th>Raw Change</th>
-                    <th>% Change</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Num Weeks</td>
-                    <td>{dataSegmentOneValues.length}</td>
-                    <td>{dataSegmentTwoValues.length}</td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                  {[
-                    ["Sum", sum],
-                    ["Max", max],
-                    ["Mean", mean],
-                    ["Median", median],
-                    ["Mode", mode],
-                    ["Min", min],
-                  ].map((pair, i) => {
-                    const [label, callback] = pair;
-                    let segOneValue = callback.call(
-                      undefined,
-                      dataSegmentOneValues
-                    );
-                    let segTwoValue = callback.call(
-                      undefined,
-                      dataSegmentTwoValues
-                    );
-                    let delta = segTwoValue - segOneValue;
+      <section>
+        <div
+          style={{
+            width: `100%`,
+            maxWidth: `1040px`,
+            display: `flex`,
+            flexFlow: `row wrap`,
+            margin: `auto`,
+          }}
+        >
+          <h1>{chartTitle}</h1>
+          <BarTime dataset={activeData} width={800} height={300} />
+          <aside
+            style={{
+              display: `flex`,
+              flexFlow: `row wrap`,
+              justifyContent: `center`,
+              padding: `.5rem 1rem`,
+              margin: `auto`,
+            }}
+          >
+            <ColorLabel dataset={activeData} />
+          </aside>
+        </div>
+      </section>
+      <section>
+        <div
+          style={{
+            width: `100%`,
+            maxWidth: `1040px`,
+            display: `flex`,
+            flexFlow: `row wrap`,
+            margin: `auto`,
+          }}
+        >
+          <div style={{ width: "48%" }}>
+            <BarTime
+              width={400}
+              height={200}
+              dataset={dataSegmentOne}
+              fullRange={fullRange}
+              timeTickFormat={(t) => moment(t).format("M/D")}
+            />
+          </div>
+          <div style={{ width: "48%" }}>
+            <BarTime
+              width={400}
+              height={200}
+              dataset={dataSegmentTwo}
+              fullRange={fullRange}
+              timeTickFormat={(t) => moment(t).format("M/D")}
+            />
+          </div>
+          <h2 style={{ textAlign: `center` }}>
+            {chartTitle} | Simple Stat Breakdown
+          </h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Stat</th>
+                <th>{`${segmentOneStart.format(
+                  "L"
+                )} - ${segmentOneEnd.format("L")}`}</th>
+                <th>{`${segmentTwoStart.format(
+                  "L"
+                )} - ${segmentTwoEnd.format("L")}`}</th>
+                <th>Raw Change</th>
+                <th>% Change</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Num Weeks</td>
+                <td>{dataSegmentOneValues.length}</td>
+                <td>{dataSegmentTwoValues.length}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              {[
+                ["Sum", sum],
+                ["Max", max],
+                ["Mean", mean],
+                ["Median", median],
+                ["Mode", mode],
+                ["Min", min],
+              ].map((pair, i) => {
+                const [label, callback] = pair;
+                let segOneValue = callback.call(
+                  undefined,
+                  dataSegmentOneValues
+                );
+                let segTwoValue = callback.call(
+                  undefined,
+                  dataSegmentTwoValues
+                );
+                let delta = segTwoValue - segOneValue;
 
-                    return (
-                      <tr key={i}>
-                        <td>{label}</td>
-                        <td>{segOneValue.toFixed(2)}</td>
-                        <td>{segTwoValue.toFixed(2)}</td>
-                        <td>{delta.toFixed(2)}</td>
-                        <td>{`${((delta / segOneValue) * 100).toFixed(
-                          2
-                        )}%`}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </section>
-        </>
-      )}
+                return (
+                  <tr key={i}>
+                    <td>{label}</td>
+                    <td>{segOneValue.toFixed(2)}</td>
+                    <td>{segTwoValue.toFixed(2)}</td>
+                    <td>{delta.toFixed(2)}</td>
+                    <td>{`${((delta / segOneValue) * 100).toFixed(
+                      2
+                    )}%`}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 };
