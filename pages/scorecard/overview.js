320d7139a962229633c8f549565cae5b81b94b9a
Dan <danielpowell12@msn.com>
2022-08-01 08:45:27 -0600

    Checks in scorecard

---
+++ pages/scorecard/overview.js
@@ -0,0 +1,194 @@
+import * as React from "react";
+import { Layout } from "../../components";
+import styles from "./styles.module.css";
+
+import weeklyData from "./weeklyData.json";
+import yearlyData from "./yearlyData.json";
+
+const WEEKS_HELPER = [3, 2, 1];
+const COMPARE_YEARS = [2021, 2020, 2019, 2018];
+const THIS_YEAR = 2022;
+
+const ATTRIBUTE_NAMES = [
+  "Billed Rev",
+  "Active Families",
+  "Approved Hours",
+  "Hour per Client",
+  "Active Tutors",
+  "Online %",
+  "Salaried Coach %",
+  "SAT/ACT Hours %",
+  "Tutor Pay Rate %",
+  "Avg Online Bill Rate",
+  "Avg In-Person Bill Rate",
+  "Client Referrals",
+  "Contact Us Forms",
+  "Projects Added",
+  "Consultations",
+];
+
+const asDecimal = new Intl.NumberFormat("en-US", {
+  style: "decimal",
+});
+const asPercent = new Intl.NumberFormat("en-US", {
+  style: "percent",
+  minimumFractionDigits: 2,
+});
+const asDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  currencySign: "accounting",
+});
+
+const FORMATTERS = {
+  "Billed Rev": asDollar,
+  "Active Families": asDecimal,
+  "Approved Hours": asDecimal,
+  "Hour per Client": asDecimal,
+  "Active Tutors": asDecimal,
+  "Online %": asPercent,
+  "Salaried Coach %": asPercent,
+  "SAT/ACT Hours %": asPercent,
+  "Tutor Pay Rate %": asPercent,
+  "Avg Online Bill Rate": asDollar,
+  "Avg In-Person Bill Rate": asDollar,
+  "Client Referrals": asDecimal,
+  "Contact Us Forms": asDecimal,
+  "Projects Added": asDecimal,
+  Consultations: asDecimal,
+};
+
+const ByWeek = () => {
+  const [dataset, setDataset] = React.useState(weeklyData);
+  const [compareYear, setCompareYear] = React.useState(
+    COMPARE_YEARS[COMPARE_YEARS.length - 1]
+  );
+
+  const activeWeek = dataset[dataset.length - 1];
+  const activeWeekNum = activeWeek["SY Week"];
+
+  const recentWeeks = WEEKS_HELPER.map(
+    (weekDiff) => dataset[dataset.length - weekDiff]
+  );
+  const lastYearWeeks = recentWeeks.map((weekData) =>
+    dataset.find(
+      (lastYear) =>
+        lastYear["SY Week"] === weekData["SY Week"] &&
+        lastYear["SY Year"] === compareYear
+    )
+  );
+
+  return (
+    <Layout showNav={false}>
+      <h1>Scorecard Overview</h1>
+      <div className={styles.filters}>
+        <div className={styles.filterGroup}>
+          Dataset
+          {[
+            ["By Week", "weeklyData", weeklyData],
+            ["SYTD", "yearlyData", yearlyData],
+          ].map(([title, id, data]) => (
+            <div key={id}>
+              <input
+                id={id}
+                type="radio"
+                onChange={() => setDataset(data)}
+                checked={data == dataset}
+              />
+              <label htmlFor={id}>{title}</label>
+            </div>
+          ))}
+        </div>
+        <div className={styles.filterGroup}>
+          Year Compare To
+          {COMPARE_YEARS.map((year) => (
+            <div key={year}>
+              <input
+                id={year}
+                type="radio"
+                onChange={() => setCompareYear(year)}
+                checked={year == compareYear}
+              />
+              <label htmlFor={year}>{year}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Attribute</td>
+            {recentWeeks.map((week, weekIndex) => (
+              <td key={weekIndex}>
+                Wk {week["SY Week"]} - {THIS_YEAR}
+              </td>
+            ))}
+            <td />
+            {lastYearWeeks.map((week, weekIndex) => (
+              <td key={weekIndex}>
+                Wk {week["SY Week"]} - {compareYear}
+              </td>
+            ))}
+            <td />
+            {lastYearWeeks.map((week, weekIndex) => (
+              <td key={weekIndex}>Wk {week["SY Week"]} %</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {ATTRIBUTE_NAMES.map((attr) => {
+            const formatter = FORMATTERS[attr];
+
+            return (
+              <tr key={attr}>
+                <td>{attr}</td>
+                {recentWeeks.map((week, weekIndex) => (
+                  <td
+                    key={weekIndex}
+                    className={
+                      week["SY Week"] == activeWeekNum && styles.activeWeek
+                    }
+                  >
+                    {formatter.format(week[attr])}
+                  </td>
+                ))}
+                <td></td>
+                {lastYearWeeks.map((week, weekIndex) => (
+                  <td
+                    key={weekIndex}
+                    className={
+                      week["SY Week"] == activeWeekNum && styles.activeWeek
+                    }
+                  >
+                    {formatter.format(week[attr])}
+                  </td>
+                ))}
+                <td></td>
+                {recentWeeks.map((week, weekIndex) => {
+                  const lastYearWeek = lastYearWeeks.find(
+                    (lastWeek) => lastWeek["SY Week"] === week["SY Week"]
+                  );
+                  const thisYear = week[attr];
+                  const lastYear = lastYearWeek[attr];
+
+                  return (
+                    <td
+                      key={weekIndex}
+                      className={
+                        week["SY Week"] == activeWeekNum && styles.activeWeek
+                      }
+                    >
+                      {asPercent.format((thisYear - lastYear) / lastYear)}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+
+export default ByWeek;
